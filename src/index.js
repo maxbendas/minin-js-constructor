cccc66ab44c0999b9c337005325a01758060e32c
maxbendas <maxbendas86@gmail.com>
2021-02-12 15:32:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import {model} from './model'
+import {templates} from './templates'
+import './styles/style.css'
+
+const site = document.querySelector('#site')
+
+model.forEach(block => {
+    const toHTML = templates[block.type]
+    if(toHTML){
+        site.insertAdjacentHTML('beforeend', toHTML(block))
+    }
+})
+
+
---
+++ src/model.js
@@ -0,0 +1,12 @@
+export const model = [
+    {type: 'title', value: 'Hello World from JS'},
+    {type: 'text', value: 'here we go with some text'},
+    {
+        type: 'columns', value: [
+            '11111',
+            '22222',
+            '33333'
+        ]
+    },
+    {type:'image', value:'./assets/image.png'}
+]
